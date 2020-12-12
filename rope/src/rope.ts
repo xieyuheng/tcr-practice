5788eddb01bc4fc87d43632e0fcd85d1c11964fd
Xie Yuheng <xyheme@gmail.com>
2020-12-12 17:56:40 +0800

    ok

--- rope/src/rope.ts
+++ rope/src/rope.ts
@@ -6,7 +6,7 @@ export type Rope = {
 }
 
 export abstract class Rope2 {
-  abstract to_string(): string
+  // to_string(): string
   // substr(start: number, length: number): Rope2 {
   //   return Substr(this, start, length)
   // }
