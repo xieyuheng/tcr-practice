8b9c257c060740d39f3d2ceb6c26f47bc19d817d
Xie Yuheng <xyheme@gmail.com>
2020-12-12 18:00:30 +0800

    ok

--- rope/src/rope.ts
+++ rope/src/rope.ts
@@ -34,3 +34,4 @@ export function Substr(rope: Rope, start: number, length: number): Rope {
 
 ut.assert_equal(Unit("abcde").to_string(), "abcde")
 ut.assert_equal(Unit("abcde").substr(1, 3).to_string(), Unit("bcd").to_string())
+ut.assert_equal(Unit("abcde").substr(1, 3).substr(1, 2).to_string(), Unit("cd").to_string())
