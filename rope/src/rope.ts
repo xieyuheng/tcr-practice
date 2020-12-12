f927ea7cc6767344a83ca1c974bea30beff10ccf
Xie Yuheng <xyheme@gmail.com>
2020-12-12 17:26:35 +0800

    ok

--- rope/src/rope.ts
+++ rope/src/rope.ts
@@ -2,18 +2,22 @@ import * as ut from "./ut"
 
 export type Rope = {
   to_string: () => string
-  substring: (start: number, end: number) => Rope
+  substr: (start: number, length: number) => Rope
 }
 
 export function Rope(str: string): Rope {
   return {
     to_string: () => str,
-    substring: (start, end) => Rope("bcd")
+    substr: (start, length) => Rope("bcd")
   }
 }
 
-// export function Substring(rope: Rope)
+// export function substr(rope: Rope, start: number, length: number): Rope {
+//   return {
+//     start: number, length: number
+//   }
+// }
 
 ut.assert_equal(Rope("abc").to_string(), "abc")
 ut.assert_equal(Rope("abcde").to_string(), "abcde")
-ut.assert_equal(Rope("abcde").substring(1, 3).to_string(), Rope("bcd").to_string())
+ut.assert_equal(Rope("abcde").substr(1, 3).to_string(), Rope("bcd").to_string())
