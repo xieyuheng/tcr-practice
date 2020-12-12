7153ec107ffc205d85c87ee9f4e8480a71c19e58
Xie Yuheng <xyheme@gmail.com>
2020-12-12 16:56:12 +0800

    ok

--- rope/src/index.ts
+++ rope/src/index.ts
@@ -1,7 +1,13 @@
+import assert from "assert"
+
 export type Rope = {
   str: string
+  to_string: string
 }
 
 export function Rope(str: string): Rope {
-  return { str }
+  return {
+    str,
+    to_string: str,
+  }
 }
