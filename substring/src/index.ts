56577ec00e2886b882cab1754853d6fefa833a59
Xie Yuheng <xyheme@gmail.com>
2020-12-12 20:32:25 +0800

    ok

--- substring/src/index.ts
+++ substring/src/index.ts
@@ -6,7 +6,10 @@ export function double_string_take(
   start: number,
   end: number
 ): string {
-  return "cde"
+  const new_left = left.substring(start, end)
+  const length = new_left.length
+  const new_right = right.substring(start - length, end - length)
+  return new_left.concat(new_right)
 }
 
 // ut.assert_equal(double_string_take("abc", "def", 2, 5), "cde")
