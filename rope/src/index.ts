e87c5db5898c2a93cf6ac9beb2ab818958a2bbc4
Xie Yuheng <xyheme@gmail.com>
2020-12-12 16:57:05 +0800

    ok

--- rope/src/index.ts
+++ rope/src/index.ts
@@ -2,12 +2,14 @@ import assert from "assert"
 
 export type Rope = {
   str: string
-  to_string: string
+  to_string: () => string
 }
 
 export function Rope(str: string): Rope {
   return {
     str,
-    to_string: str,
+    to_string: () => str,
   }
 }
+
+assert(Rope("abc").to_string() === "abc")
