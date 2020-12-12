af4b2b76d5b046952c6833a0dcc78bb5390c0686
Xie Yuheng <xyheme@gmail.com>
2020-12-12 17:06:43 +0800

    up

---
+++ rope/src/rope.ts
@@ -0,0 +1,16 @@
+import * as ut from "./ut"
+
+export type Rope = {
+  str: string
+  to_string: () => string
+}
+
+export function Rope(str: string): Rope {
+  return {
+    str,
+    to_string: () => str,
+  }
+}
+
+ut.assert_equal(Rope("abc").to_string(), "abc")
+ut.assert_equal(Rope("abcde").to_string(), "abcde")
