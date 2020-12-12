6326a18a49614db95421b1930cfcb115ee973e96
Xie Yuheng <xyheme@gmail.com>
2020-12-12 19:37:42 +0800

    ok

--- rope/src/rope.ts
+++ rope/src/rope.ts
@@ -50,14 +50,7 @@ export const Concat = (left: Rope, right: Rope) =>
   })
 
 ut.assert_equal(Unit("abcde").to_string(), "abcde")
-
 ut.assert_equal(Unit("abcde").substr(1, 3).to_string(), "bcd")
-
 ut.assert_equal(Unit("abcde").substr(1, 3).substr(1, 2).to_string(), "cd")
-
 ut.assert_equal(Unit("abc").concat(Unit("de")).to_string(), "abcde")
-
-// ut.assert_equal(
-//   Unit("abcde").delete(1, 3).to_string(),
-//   "ae"
-// )
+// ut.assert_equal(Unit("abcde").delete(1, 3).to_string(), "ae")
